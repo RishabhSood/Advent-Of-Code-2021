016757447ba8de27c94a91e5a5e0f9e78f1d70f7
RishabhSood <55499929+RishabhSood@users.noreply.github.com>
2021-12-19 08:52:30 +0530

    Completed

--- Day4_1.cpp
+++ Day4_1.cpp
@@ -38,10 +38,78 @@ void processBoards(vector<vector<vector<int>>> &boards)
     }
 }
 
+void updateBoards(vector<vector<vector<int>>> &boards, int num)
+{
+    for(int idx = 0; idx < boards.size(); idx++)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                if(boards[idx][i][j] == num)
+                {
+                    boards[idx][i][j] = -1;
+                }   
+            }
+        }
+    }
+}
+
+int winNum(vector<vector<vector<int>>> boards)
+{
+    for(auto board : boards)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            if(board[i][i] == -1)
+            {
+                int sumR = 0;
+                int sumC = 0;
+                for(int j = 0; j < 5; j++)
+                {
+                    sumR += board[i][j];
+                    sumC += board[j][i];
+                }
+                if(sumR == -5 || sumC == -5)
+                {
+                    int ans = 0;
+                    for(auto row : board)
+                    {
+                        for(auto elem : row)
+                        {
+                            if(elem != -1)
+                                ans += elem;
+                        }
+                    }
+                    return ans;
+                }
+            }
+        }
+    }
+
+    return -1;
+}
+
+void bingo(vector<int> calledNums, vector<vector<vector<int>>> boards)
+{
+    for(auto num : calledNums)
+    {
+        updateBoards(boards, num);
+        int win = winNum(boards);
+        if(win != -1)
+        {
+            cout << num << " * " << win;
+            break;
+        }
+    }
+}
+
 int main() {
     vector<int> calledNums;
     vector<vector<vector<int>>> boards;
 
     processNums(calledNums);
     processBoards(boards);
+
+    bingo(calledNums, boards);
 }

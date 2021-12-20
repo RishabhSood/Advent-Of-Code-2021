f11c96e2ccf761dceb83b7a9dde9aa9720f6ea9c
RishabhSood <55499929+RishabhSood@users.noreply.github.com>
2021-12-20 13:39:45 +0530

    Create Day11_1.cpp

---
+++ Day11_1.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    vector<vector<int>> octopus;
+    
+    while(getline(cin, s))
+    {
+        vector<int> row;
+        for(auto c : s)
+        {
+            row.push_back(c - '0');
+        }
+        octopus.push_back(row);
+    }
+
+    int m = octopus.size();
+    int n = octopus[0].size();
+
+    int flash = 0;
+
+    for(int i = 0; i < 100; i++)
+    {
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                // child elements affected by parent flashes
+                stack<pair<int, int>> st;
+                st.push({i, j});
+
+                while (st.size() > 0)
+                {
+                    auto p = st.top();
+                    st.pop();
+                    int x = p.first;
+                    int y = p.second;
+                    // each element gets one energy point (either default or from the previous flash)
+                    octopus[x][y]++;
+                    // only one flash permitted
+                    if (octopus[x][y] == 10)
+                    {
+                        flash++;
+                        // traverse through neighbors (including diagonal)
+                        for(int ni = -1; ni <= 1; ni++)
+                        {
+                            for(int nj = -1; nj <= 1; nj++)
+                            {
+                                int nx = x + ni;
+                                int ny = y + nj;
+                                // range bound check
+                                if(nx >= 0 && nx < m && ny >= 0 && ny < n)
+                                {
+                                    // add valid neighbor to processing stack
+                                    st.push({nx, ny});
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(octopus[i][j] > 9)
+                    octopus[i][j] = 0;
+            }
+        }
+    }
+    cout << flash;
+  
+    return 0;
+}
